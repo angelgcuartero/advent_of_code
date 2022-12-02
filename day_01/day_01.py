31066ae9f5595c1ff26ddade9f76d697e2fb668e
Ángel García <anhelido@gmail.com>
2022-12-02 20:50:50 +0100

    feat: Add day 01 problem

---
+++ day_01/day_01.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+# Advent of Code 2022
+# Day 1
+
+from collections import Counter
+import os
+
+file_path = file_path =input_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "input_day_01.txt") 
+
+
+def read_file(input_file: str) -> str:
+    with open(input_file) as f:
+        lines = f.readlines()
+    return lines
+
+
+def get_count(lines: str) -> dict:
+    result_dict = {}
+
+    elf = 1
+    for i, line in enumerate(lines, start=1):
+        stripped_line = line.strip()
+        if stripped_line:
+            result_dict[elf] = result_dict.get(elf, 0) + int(stripped_line)
+        else:            
+            elf += 1
+
+    return result_dict
+
+
+def main() -> int:
+    lines = read_file(file_path)
+    result = get_count(lines)
+    c = Counter(result)
+    elf = c.most_common(1)[0][0]
+    calories = c.most_common(1)[0][1]
+    print(f"The elf with the most calories is number {elf}, carrying {calories}")
+    top_3 = c.most_common(3)
+    print (f"The top 3 elves' calories sum up: {sum([x[1] for x in top_3])}")
+    return 0
+
+if __name__ == "__main__":
+    exit(main())
---
+++ day_01/test_day_01.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Advent of Code 2022
+# Day 1 - Test Inventory
+
+from collections import Counter
+import os
+import pytest
+import day_01 as d1
+
+file_path =input_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_input_day_01.txt") 
+
+# Part 1
+
+
+def test_count():
+    lines = d1.read_file(file_path)
+    assert len(lines) == 14
+
+
+def test_get_calories_count():
+    lines = d1.read_file(file_path)
+    result = d1.get_count(lines)
+    assert len(result) == 5
+    c = Counter(result)
+    print(c.most_common(1))
+
+def test_top_3():
+    lines = d1.read_file(file_path)
+    result = d1.get_count(lines)
+    c = Counter(result)
+    top_3 = c.most_common(3)
+    assert top_3 == [(4, 24000), (3, 11000), (5, 10000)]
+    print (sum([x[1] for x in top_3]))
+
+
+if __name__ == "__main__":
+    # os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    pytest.main()
